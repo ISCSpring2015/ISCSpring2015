8a887d23a98e4b45f2298af579eceae64bd9501f
shivachandh <meela.shivachandh@gmail.com>
2015-04-01 20:33:09 -0500

    shivachandh1

--- Spring2015/Spring2015/App_Start/Startup.Auth.cs
+++ Spring2015/Spring2015/App_Start/Startup.Auth.cs
@@ -17,7 +17,7 @@ namespace Spring2015
                 LoginPath = new PathString("/Account/Login")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);//tfdkgASkgjSV
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
