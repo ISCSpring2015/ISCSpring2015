fc291a5d97051f2fbaa5afed2a5ffeb4117c9dfe
bnandini <nandini.bellamkonda@gmail.com>
2015-04-22 23:48:54 -0500

    validations in section,skills,skillset,subskill

--- Spring2015/Spring2015/Skill.cs
+++ Spring2015/Spring2015/Skill.cs
@@ -11,6 +11,7 @@ namespace Spring2015
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Skill
     {
@@ -22,8 +23,11 @@ namespace Spring2015
         public int SkillID { get; set; }
         public int SkillsetID { get; set; }
         public int SkillSetNum1 { get; set; }
+        [Display(Name="SkillNumber")]
         public int SkillNum2 { get; set; }
+         [Required(ErrorMessage = "**Please Provide your Name", AllowEmptyStrings = false)]
         public string Name { get; set; }
+         [Required(ErrorMessage = "**Please Provide your ShortName", AllowEmptyStrings = false)]
         public string ShortName { get; set; }
     
         public virtual SkillSet SkillSet { get; set; }
